0d26612fbca15003c765c3e62d5249947db9c9e4
uelionobre <uelionobre@hotmail.com>
2024-05-31 15:25:04 -0300

    Add rota /

--- src/routes/home.router.js
+++ src/routes/home.router.js
@@ -2,6 +2,8 @@ const { Router } = require('express');
 
 const router = Router();
 
-router.get('/', (req, res) => res.json({ message: 'Hello world' }));
+router.get('/', (_, res) => res.json({ message: 'Hello world' }));
+router.get('/error', (req, res) => { throw new Error('Rota /error gerou um erro') });
+router.use('*', (req, res) => { throw new Error('Rota não implementada', { cause: { statusCode: 404 } }) });
 
 module.exports = router;
