9c213504db20457cb2dbd3903781af9bfe851835
uelionobre <uelionobre@hotmail.com>
2024-05-31 19:35:38 -0300

    Adiciona rota de usuários

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 
 const homeRouter = require('./home.router');
+const usersRouter = require('./users.router');
 
 const router = Router();
 
 router.use('/', homeRouter);
-router.use('/*', (req, res) => { throw new Error('Rota não implementada', { cause: { statusCode: 404 } }) })
+router.use('/users', usersRouter);
+// router.use('/*', (req, res) => { throw new Error('Rota não implementada', { cause: { statusCode: 404 } }) })
 
 module.exports = router;
