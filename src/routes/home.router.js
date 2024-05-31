3aae9f0d09f6557a57940e30c5e1a0058f8ecca4
uelionobre <uelionobre@hotmail.com>
2024-05-31 15:01:12 -0300

    Configuração de rota /

---
+++ src/routes/home.router.js
@@ -0,0 +1,7 @@
+const { Router } = require('express');
+
+const router = Router();
+
+router.get('/', (req, res) => res.json({ message: 'Hello world' }));
+
+module.exports = router;
---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+const { Router } = require('express');
+
+const homeRouter = require('./home.router');
+
+const router = Router();
+
+router.use('/', homeRouter);
+
+module.exports = router;
