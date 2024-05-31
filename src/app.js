f1d45615bfaa3e800c2aad495a38dc8dc27b493b
uelionobre <uelionobre@hotmail.com>
2024-05-31 15:25:58 -0300

    Adiciona helmet e middleware de erro

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
+require('express-async-errors')
+
+const helmet = require('helmet');
 const cors = require('cors');
 
 const routes = require('./routes');
 const app = express();
+const errorHandler = require('./middlewares/error.middleware');
 
 app.use(cors());
+app.use(helmet());
 app.use(express.json());
 app.use(routes);
+app.use(errorHandler);
 
 module.exports = app;
