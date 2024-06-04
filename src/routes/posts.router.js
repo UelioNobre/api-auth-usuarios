8f7be2e6ab03f99cbe1d30400e1648dfee46f8da
uelionobre <uelionobre@hotmail.com>
2024-06-04 10:28:39 -0300

    Verifica se o usuário é dono do post

--- src/routes/posts.router.js
+++ src/routes/posts.router.js
@@ -1,10 +1,22 @@
 const { Router } = require('express');
+const postsMiddleware = require('../middlewares/post.middleware');
 const postsController = require('../controllers/posts.controller');
 
 const router = Router();
 
 router.get('/', postsController.Home);
 router.post('/', postsController.Create);
-router.put('/:postID', postsController.Update);
+
+router.get(
+  '/:postID',
+  postsMiddleware.checkPostOwnership,
+  postsController.Read
+);
+
+router.put(
+  '/:postID',
+  postsMiddleware.checkPostOwnership,
+  postsController.Update
+);
 
 module.exports = router;
