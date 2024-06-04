17cbdbe0f5c2157cea91bcac7c9230d074539cbe
uelionobre <uelionobre@hotmail.com>
2024-06-04 14:00:16 -0300

    Adiciona middleware para verificar se o usuário esta ativo.

--- src/middlewares/user.middleware.js
+++ src/middlewares/user.middleware.js
@@ -20,7 +20,20 @@ function userNotChangePassword(req, _res, next) {
   return next();
 }
 
+async function userIsActive(req, res, next) {
+  const { token } = req;
+  const user = await UserModel.findById(token.data._id).select('isActive') || false;
+
+
+  if (!user.isActive) {
+    return next(new Error('Usuário não encontrado', { cause: { statusCode: 404 } }));
+  }
+
+  return next();
+}
+
 module.exports = {
   userExists,
-  userNotChangePassword
+  userNotChangePassword,
+  userIsActive
 };
