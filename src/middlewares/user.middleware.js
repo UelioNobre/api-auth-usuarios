489a6fad596fc80f81e3c5e5072dcf02c9e96b75
uelionobre <uelionobre@hotmail.com>
2024-06-04 12:17:43 -0300

    Middleware de usuario se existe e não permite alterar o próprio e-mail

---
+++ src/middlewares/user.middleware.js
@@ -0,0 +1,26 @@
+const UserModel = require("../models/user.model");
+
+async function userExists(req, _res, next) {
+  const { token } = req;
+  const user = await UserModel.findById(token.data._id) || false;
+
+  if (!user) {
+    return next(new Error('Usuário não encontrado', { cause: { statusCode: 404 } }));
+  }
+
+  return next();
+}
+
+function userNotChangePassword(req, _res, next) {
+  console.log(req.body);
+  if (Object.keys(req.body).includes('email')) {
+    return next(new Error('Não é permitido alterar o email de cadastro', { cause: { statusCode: 403 } }));
+  }
+
+  return next();
+}
+
+module.exports = {
+  userExists,
+  userNotChangePassword
+};
