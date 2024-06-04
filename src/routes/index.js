326b0b3af73aff217175a97eb1fa11e8d26318d4
uelionobre <uelionobre@hotmail.com>
2024-06-04 09:23:24 -0300

    Adiciona rotas para adicionar e listar posts

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,17 @@ const { Router } = require('express');
 const homeRouter = require('./home.router');
 const usersRouter = require('./users.router');
 const authRouter = require('./auth.router');
+const postsRouter = require('./posts.router');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 const router = Router();
 
 router.use('/', homeRouter);
-
 router.use('/auth', authRouter);
 router.use('/users', usersRouter);
+
+router.use(authMiddleware);
+router.use('/posts', postsRouter);
 // router.use('/*', (req, res) => { throw new Error('Rota não implementada', { cause: { statusCode: 404 } }) })
 
 module.exports = router;
