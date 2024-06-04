23468c94bd52b466607ceb981151a385727a4dca
uelionobre <uelionobre@hotmail.com>
2024-06-04 10:35:43 -0300

    Adiciona erro para usuário não autenticado

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,15 +2,19 @@ const { decodeToken } = require("../utils/tokenizer");
 
 function authMiddleware(req, _res, next) {
   try {
-    const header = req.headers.authorization || "";
+    const header = req.headers.authorization || false;
+
+    if (!header) {
+      next(new Error('Usuário não autenticado', { cause: { statusCode: 401 } }));
+    }
+
     const authorization = header.trim();
     const token = decodeToken(authorization.replace('Bearer ', ''));
 
     req.token = token;
     next();
   } catch (_error) {
-    const erro = new Error('Token inválido', { cause: { statusCode: 400 } });
-    next(erro);
+    next(new Error('Token inválido', { cause: { statusCode: 400 } }));
   }
 }
 
