d9305851d177e69c3a80edc6b19ed194c2264a2e
uelionobre <uelionobre@hotmail.com>
2024-06-04 12:18:15 -0300

    Adiciona proteção as rotas de alteração de dados de usuarios

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,12 +1,28 @@
 const { Router } = require('express');
 const userController = require('../controllers/users.controller');
+const userMiddleware = require('../middlewares/user.middleware');
 
 const router = Router();
 
-router.get('/', userController.List);
-router.post('/', userController.Create);
-router.get('/:id', userController.Read);
-router.put('/:id', userController.Update);
+
+router.post(
+  '/',
+  userMiddleware.userExists,
+  userController.Create
+);
+
+router.get(
+  '/',
+  userMiddleware.userExists,
+  userController.Read
+);
+
+router.put(
+  '/',
+  userMiddleware.userNotChangePassword,
+  userController.Update
+);
+
 router.delete('/:id', userController.Delete);
 
 module.exports = router;
